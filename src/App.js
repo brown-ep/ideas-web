eb09b248bc1aa8a0db619de4d8681d8b4fc42278
Kevin Xiang <kevinxiang3981@gmail.com>
2018-11-06 14:07:22 -0500

    Added Routing
    login auth renamed loginPage and moved to new pages directory

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React, { Component } from 'react'
-import { firebaseConnect } from 'react-redux-firebase'
-import { connect } from 'react-redux'
-import './App.css'
-import Login from './Login'
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/loginPage';
 
 class App extends Component {
-  handleClick = () => {
-    this.props.firebase.login({
-      provider: 'google',
-      type: 'redirect',
-    })
-  }
-  render() {
-    return (
-      <div className="App bg-grey-light">
-        <header className="App-header py-20">
-          <div className="container mx-auto">
-            <h1 className="text-5xl text-white pb-2">Have an idea?</h1>
-            {/* {this.props.auth ? (
-              <div>You are {this.props.profile()}</div>
-            ) : (
-              <button
-                onClick={this.handleClick}
-                class="text-xl text-blue bg-white rounded-sm p-2 font-bold"
-              >
-                Login
-              </button>
-            )} */}
-          </div>
-        </header>
-        <Login />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route exact={true} path='/' render={() => (
+                        <div className="App">
+                            <Login />
+                        </div>
+                    )} />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
-export default firebaseConnect()(
-  connect(state => ({
-    auth: state.firebase.auth,
-    profile: state.firebase.profile,
-  }))(App)
-)
+export default App;
